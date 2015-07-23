bd18e1dd81cdb2a20856b5e36a39f85cbfaf1f51
themrburrelly <themrburrelly@gmail.com>
2015-07-24 00:13:21 +0200

    obrir i tancar la llum a partir de una url

--- llum/views.py
+++ llum/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request, state):
+    if int(state) == 0:
+        context = {"msg": "La llum esta oberta"}
+    else:
+        context = {"msg": "La llum esta apagada"}
+    return render(request, 'llum/index.html', context)
