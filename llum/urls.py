51e90099a14171fcaed0f6a5b82846f87dc43ac7
themrburrelly <themrburrelly@gmail.com>
2015-07-25 22:43:37 +0200

    Arreglat el problema d'obrir i tancar

--- llum/urls.py
+++ llum/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<state>.*?)/$', views.index, name='index'),
+    url(r'^$', views.index, name='index1'),
+    url(r'^(?P<state>.*?)/$', views.index, name='index2'),
 ]
--- llum/views.py
+++ llum/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 import RPi.GPIO as GPIO
 
 
-def index(request, state):
+def index(request, state=1):
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(2, GPIO.OUT)
     if int(state) == 0:
-        GPIO.output(2, GPIO.HIGH)
+        GPIO.output(2, GPIO.LOW)
         context = {"msg": "La llum esta oberta"}
     else:
-        GPIO.output(2, GPIO.LOW)
+        GPIO.output(2, GPIO.HIGH)
         context = {"msg": "La llum esta apagada"}
     return render(request, 'llum/index.html', context)
