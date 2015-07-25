4e7f3889753619e57d3df0a3239a235a1b96cad6
themrburrelly <themrburrelly@gmail.com>
2015-07-25 19:25:06 +0200

    control relay rbp

--- llum/views.py
+++ llum/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+import RPi.GPIO as GPIO
 
 
 def index(request, state):
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(2, GPIO.OUT)
     if int(state) == 0:
+        GPIO.output(2, GPIO.HIGH)
         context = {"msg": "La llum esta oberta"}
     else:
+        GPIO.output(2, GPIO.LOW)
         context = {"msg": "La llum esta apagada"}
     return render(request, 'llum/index.html', context)
