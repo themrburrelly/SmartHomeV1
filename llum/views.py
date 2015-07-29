c55f07254bcf99219ebea1482e174d687417be47
themrburrelly <themrburrelly@gmail.com>
2015-07-29 18:26:59 +0200

    nova variable pin

--- llum/views.py
+++ llum/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 import RPi.GPIO as GPIO
 
+pin = 7
+
 
 def index(request, state=1):
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(2, GPIO.OUT)
+    GPIO.setup(pin, GPIO.OUT)
     if int(state) == 0:
-        GPIO.output(2, GPIO.LOW)
+        GPIO.output(pin, GPIO.LOW)
         context = {"msg": "La llum esta oberta"}
     else:
-        GPIO.output(2, GPIO.HIGH)
+        GPIO.output(pin, GPIO.HIGH)
         context = {"msg": "La llum esta apagada"}
     return render(request, 'llum/index.html', context)
