21945b24a5598a78e51700591b764835f07c099f
themrburrelly <themrburrelly@gmail.com>
2015-07-29 20:57:24 +0200

    changed name app llum to family
    this is the second change i’ve made to this folder

---
+++ family/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+# check if the code is running on the raspberry pi
+try:
+    import RPi.GPIO as GPIO
+    plataform = "rbpi"
+except:
+    plataform = "mac"
+
+    pin = 6
+
+    def index(request, state=1):
+        """Return the responce for the request."""
+        if plataform == "rbpi":
+            GPIO.setmode(GPIO.BCM), GPIO.setup(pin, GPIO.OUT)
+        if int(state) == 0:
+            if plataform == "rbpi":
+                GPIO.output(pin, GPIO.LOW)
+            context = {"msg": "La llum esta obert"}
+        else:
+            if plataform == "rbpi":
+                GPIO.output(pin, GPIO.HIGH)
+            context = {"msg": "La llum esta apagada"}
+        return render(request, 'llum/index.html', context)
