3cd973b074f437306aa254ed9092cff02baf178c
themrburrelly <themrburrelly@gmail.com>
2015-07-29 18:38:36 +0200

    canvi a pin 6

--- llum/views.py
+++ llum/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import RPi.GPIO as GPIO
 
-pin = 7
+pin = 6
 
 
 def index(request, state=1):
@@ -9,7 +9,7 @@ def index(request, state=1):
     GPIO.setup(pin, GPIO.OUT)
     if int(state) == 0:
         GPIO.output(pin, GPIO.LOW)
-        context = {"msg": "La llum esta oberta"}
+        context = {"msg": "La llum esta obert"}
     else:
         GPIO.output(pin, GPIO.HIGH)
         context = {"msg": "La llum esta apagada"}
