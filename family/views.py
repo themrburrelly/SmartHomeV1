58188c575be39acfca33d4903abb2bc5548030df
themrburrelly <themrburrelly@gmail.com>
2015-07-30 13:34:22 +0200

    added sensor code

--- family/views.py
+++ family/views.py
@@ -6,18 +6,22 @@ try:
 except:
     plataform = "mac"
 
-    pin = 6
+    relay_pin = 9
+    sensor_pin = 10
 
     def index(request, state=1):
         """Return the responce for the request."""
         if plataform == "rbpi":
-            GPIO.setmode(GPIO.BCM), GPIO.setup(pin, GPIO.OUT)
-        if int(state) == 0:
+            # set up raspberry pins
+            GPIO.setmode(GPIO.BCM)
+            GPIO.setup(relay_pin, GPIO.OUT)
+            GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUT_DOWN)
+        if int(state) == 0 or GPIO.input():
             if plataform == "rbpi":
-                GPIO.output(pin, GPIO.LOW)
+                GPIO.output(relay_pin, GPIO.LOW)
             context = {"msg": "La llum esta obert"}
         else:
             if plataform == "rbpi":
-                GPIO.output(pin, GPIO.HIGH)
+                GPIO.output(relay_pin, GPIO.HIGH)
             context = {"msg": "La llum esta apagada"}
         return render(request, 'family/index.html', context)
