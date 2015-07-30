0e69084e82fc9f97a2e4608d0e2e081221b263df
themrburrelly <themrburrelly@gmail.com>
2015-07-30 14:01:04 +0200

    small changes

--- family/views.py
+++ family/views.py
@@ -5,18 +5,17 @@ try:
     plataform = "rbpi"
     relay_pin = 9
     sensor_pin = 10
+    # set up raspberry pins
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(relay_pin, GPIO.OUT)
+    GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(sensor_pin, GPIO.BOTH, bouncetime=150)
 except:
     plataform = "mac"
 
 
 def index(request, state=1):
     """Return the responce for the request."""
-    if plataform == "rbpi":
-        # set up raspberry pins
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(relay_pin, GPIO.OUT)
-        GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(sensor_pin, GPIO.BOTH, bouncetime=150)
     if int(state) == 0 or GPIO.input(sensor_pin):
         if plataform == "rbpi":
             GPIO.output(relay_pin, GPIO.LOW)
