1a012b11b2146d6650d9f1be47b507ef76236040
themrburrelly <themrburrelly@gmail.com>
2015-07-30 13:38:21 +0200

    corrected wrong indent

--- family/views.py
+++ family/views.py
@@ -3,25 +3,25 @@ from django.shortcuts import render
 try:
     import RPi.GPIO as GPIO
     plataform = "rbpi"
+    relay_pin = 9
+    sensor_pin = 10
 except:
     plataform = "mac"
 
-    relay_pin = 9
-    sensor_pin = 10
 
-    def index(request, state=1):
-        """Return the responce for the request."""
+def index(request, state=1):
+    """Return the responce for the request."""
+    if plataform == "rbpi":
+        # set up raspberry pins
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(relay_pin, GPIO.OUT)
+        GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUT_DOWN)
+    if int(state) == 0 or GPIO.input():
         if plataform == "rbpi":
-            # set up raspberry pins
-            GPIO.setmode(GPIO.BCM)
-            GPIO.setup(relay_pin, GPIO.OUT)
-            GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUT_DOWN)
-        if int(state) == 0 or GPIO.input():
-            if plataform == "rbpi":
-                GPIO.output(relay_pin, GPIO.LOW)
-            context = {"msg": "La llum esta obert"}
-        else:
-            if plataform == "rbpi":
-                GPIO.output(relay_pin, GPIO.HIGH)
-            context = {"msg": "La llum esta apagada"}
-        return render(request, 'family/index.html', context)
+            GPIO.output(relay_pin, GPIO.LOW)
+        context = {"msg": "La llum esta obert"}
+    else:
+        if plataform == "rbpi":
+            GPIO.output(relay_pin, GPIO.HIGH)
+        context = {"msg": "La llum esta apagada"}
+    return render(request, 'family/index.html', context)
