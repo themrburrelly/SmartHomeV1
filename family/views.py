c63c55e744ca17d504a3597ea07d38e0410dbdd3
themrburrelly <themrburrelly@gmail.com>
2015-08-24 03:19:53 +0200

    db works but connections aren't handled properly

--- family/views.py
+++ family/views.py
@@ -1,29 +1,16 @@
 from django.shortcuts import render
 from pymongo import MongoClient
-import datetime
-from time import sleep
+
 
 # Start conection with mongo db server
 client = MongoClient()
 db = client.db
-# check if the code is running on the raspberry pi
-try:
-    import RPi.GPIO as GPIO
-    plataform = "rbpi"
-    relay_pin = 4
-    sensor_pin = 10
-    # set up raspberry pins
-    GPIO.setmode(GPIO.BCM)
-    for pin in range(2, 10):
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, GPIO.HIGH)
-    GPIO.setup(relay_pin, GPIO.OUT)
-    GPIO.setup(sensor_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-except:
-    plataform = "mac"
 
 
-def index(request, state=1):
+def index(request):
     home_elements = db.home_elements
-    context = home_elements.find()
+    context = home_elements.find_one()
+    client.close()
     return render(request, 'family/index.html', context)
+
+
