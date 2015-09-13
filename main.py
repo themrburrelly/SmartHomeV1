4137030b4cdec1b7aecef537586ec9d0a38b415d
themrburrelly <themrburrelly@gmail.com>
2015-09-13 17:42:03 +0200

    motion senor code

--- main.py
+++ main.py
@@ -17,10 +17,19 @@ for pin in range(2, 10):
 
 # LOOP
 # ---------------------------------
+count = 0
+motion_sensor_pin = 10
 while True:
+    if GPIO.input(motion_sensor_pin):
+        count = 0
+        outputs.update({'name': 'right_light'}, {"$set": {'state': 1}}, upsert=False)
+    if count == 600:
+        outputs.update({'name': 'right_light'}, {"$set": {'state': 0}}, upsert=False)
     for output in outputs.find():
         if output['state'] == 0:
             GPIO.output(output['pin'], GPIO.HIGH)
         else:
             GPIO.output(output['pin'], GPIO.LOW)
+    if count < 650:
+        count += 1
     sleep(1)
